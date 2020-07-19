890c4300d78943cc4f55c6d2130203d23ab8ec6d
InseokChoi <kr.chis@gmail.com>
2020-07-19 19:22:27 +0900

    kafka listener Test

---
+++ src/main/java/kr/chis/cismsashop/shop/event/ShopEventListener.java
@@ -0,0 +1,20 @@
+package kr.chis.cismsashop.shop.event;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author InSeok
+ * Date : 2020/07/19
+ * Remark : Shop
+ */
+@Component
+public class ShopEventListener {
+
+    @KafkaListener(topics = "msaorder")
+    public void receiveOrderedEvent(ConsumerRecord consumerRecord){
+        System.out.println("=================" + consumerRecord.value());
+
+    }
+}
