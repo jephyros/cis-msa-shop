b50a79a49affe7a06a382720c32d3c56f7057c38
InseokChoi <kr.chis@gmail.com>
2020-07-19 16:48:52 +0900

    first commit

---
+++ src/main/java/kr/chis/cismsashop/CismsashopApplication.java
@@ -0,0 +1,13 @@
+package kr.chis.cismsashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CismsashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CismsashopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/chis/cismsashop/CismsashopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.chis.cismsashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CismsashopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
