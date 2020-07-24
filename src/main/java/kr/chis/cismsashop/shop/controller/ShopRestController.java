1198ee0e8331295fde1fa8a0b61788b929465342
CIS-Mac_BW <kr.chis@gmail.com>
2020-07-24 14:28:46 +0900

    Mono  LoadTest..

--- src/main/java/kr/chis/cismsashop/shop/controller/ShopRestController.java
+++ src/main/java/kr/chis/cismsashop/shop/controller/ShopRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * @author InSeok
@@ -37,5 +38,28 @@ public class ShopRestController {
         log.info("GetShop Called");
         return shopService.findById(id);
     }
+    //test=======================================================
+    @GetMapping("/service1")
+    public Mono<String> service1(String req) throws InterruptedException {
+        //netty 는 비동기로 mono 로 해야 속도가 날듯
+        //Thread.sleep(2000);
+        //return req + "/service1";
+        return Mono.fromCallable(()-> {
+                    Thread.sleep(2000);
+                    return req + "/service1";
+                }).subscribeOn(Schedulers.elastic()).log();
+    }
+    @GetMapping("/service2")
+    public Mono<String> service2(String req) throws InterruptedException {
+        //netty 는 비동기로 mono 로 해야 속도가 날듯
+//        Thread.sleep(2000);
+//        return req + "/service2";
+        return Mono.fromCallable(()-> {
+                Thread.sleep(2000);
+                return req + "/service2";
+            }).subscribeOn(Schedulers.elastic()).log();
+
+    }
+
 
 }
