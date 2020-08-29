a2e900dab9c5a0855eb6474398cef795651e9501
InseokChoi <kr.chis@gmail.com>
2020-08-30 07:03:00 +0900

    Cors Setting

--- src/main/java/kr/chis/cismsashop/config/BeanConfig.java
+++ src/main/java/kr/chis/cismsashop/config/BeanConfig.java
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * @author InSeok
@@ -22,7 +27,20 @@ public class BeanConfig {
 
         return mapper;
     }
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8190"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedHeader("*");
 
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
     //@Bean
     //    public ModelMapper modelMapper() {
     //        return new ModelMapper();
